4ce69b6d5932597260a9e43df66944da47953cc6
Ayush Priyam <ayushpriyam24@gmail.com>
2024-03-08 10:20:28 +0530

    create update listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 import Listing from "../models/listing.model.js";
+import { errorHandler } from "../utils/error.js";
 
 export const createListing = async (req, res, next) => {
     try {
@@ -22,5 +23,22 @@ export const deleteListing = async (req, res, next) => {
         res.status(200).json("Listing has been deleted");
     } catch (error) {
         next(error);
+    }
+}
+
+export const updateListing = async (req, res,next) => {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+        return next(errorHandler(404, "Listing not found"));
+    }
+    if (req.user.id!==listing.userRef) {
+        return next(errorHandler(401, "you can onlu update your own lsiting "));  
+    }
+
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true }); //if dont do new true then we only get previous data in return 
+        res.status(200).json(updatedListing);
+    } catch (error) {
+        next(error)
     }
 }
