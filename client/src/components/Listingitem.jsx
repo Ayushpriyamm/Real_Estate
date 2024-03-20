c51d6501a7c6875f97d553287161d392365d21b4
Ayush Priyam <ayushpriyam24@gmail.com>
2024-03-20 13:46:45 +0530

    complete Home Page

--- client/src/components/Listingitem.jsx
+++ client/src/components/Listingitem.jsx
@@ -3,12 +3,12 @@ import { MdLocationOn } from "react-icons/md";
 
 export const Listingitem = ({ listing }) => {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg  w-full sm:w-[330px]">
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
       <Link to={`/listing/${listing._id}`}>
         <img
           src={listing.imageUrls[0]}
           alt="listing cover"
-          className="h-[320px] sm:h[220px] w-full object-cover hover:scale-105 translate-scale duration-300"
+          className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
         <div className="p-3 flex flex-col gap-2 w-full">
           <p className="truncate text-lg font-semibold text-slate-700">
@@ -26,8 +26,8 @@ export const Listingitem = ({ listing }) => {
           <p className="text-slate-500 mt-2 font-semibold">
             ₹
             {listing.offer
-              ? listing.discountPrice.toLocaleString("en-US")
-              : listing.regularPrice.toLocaleString("en-US")}
+              ? listing.discountPrice.toLocaleString("en-IN")
+              : listing.regularPrice.toLocaleString("en-IN")}
             {listing.type === "rent" && "/month"}
           </p>
           <div className="text-slate-700 flex gap-4">
