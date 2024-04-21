d48322aef333f9c261d10ee50c71020ec0195e62
Ayush Priyam <ayushpriyam24@gmail.com>
2024-04-21 10:43:11 +0530

    adding loader in for redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,6 +6,7 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { persistor, store } from "../redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
+import LoadingSpinner from "./components/LoadingSpinner.jsx";
 
 //PersistGate is a component provided by the redux-persist library that helps ensure your application doesn't render until the persisted state has been retrieved and rehydrated.
 
@@ -15,7 +16,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
+    <PersistGate loading={<LoadingSpinner />} persistor={persistor}>
       <App />
     </PersistGate>
   </Provider>
